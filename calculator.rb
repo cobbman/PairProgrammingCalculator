37fe630b2ae02920d6e5761b1d9d34448316e6aa
William <cobbman@gmail.com>
2012-05-24 21:17:58 -0700

    added random ability to jokes

--- calculator.rb
+++ calculator.rb
@@ -18,25 +18,30 @@ class Calculator
 		jokes = {
 			'Why does The Little Mermaid wear seashells?' => 'Because D shells are too big and A shells are too small. Wakka Wakka!',
 			'Why did the fly fly?' => 'Because the spider spied her! Hoo Haa Hee Haa',
-			'What kind of movies to Pirates like to watch?' => 'Rated arrrrrrrr movies O_o tee hee'
+			'What kind of movies to Pirates like to watch?' => 'Rated arrrrrrrr movies O_o tee hee',
+			'What do you call a rabbit with fleas?' => 'Bugs Bunny!!!'
 		}
 
-    joke = jokes.keys.first
-    answer = jokes[joke]
+		# We would like to change this so that it pulls a list of jokes from a file instead of using a hash
 
-    puts joke
-    puts "Press any key to see the answer"
-    gets
-    puts answer
+	    joke = jokes.keys[rand(jokes.keys.length)]
+	    answer = jokes[joke]
+
+	    puts joke
+	    puts "Press any key to see the answer"
+	    gets
+	    puts answer
+	    puts '* * * * * * *'
 	end
 
 	def doUserChoice(choice = getUserChoice.to_i)
 		case choice
 		when 1
 			puts "you chose 1"
+			doUserChoice
 		when 2
-      tellAJoke
-      doUserChoice
+	        tellAJoke
+	        doUserChoice
 		when 3
 			puts "Goodbye"
 			Process.exit
